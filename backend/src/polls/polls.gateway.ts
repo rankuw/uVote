e801095765c0cbc95374b49f4df723c10490c5bd
Ranvijay Singh Kuwarbi <ranvijay.kuwarbi@appinventiv.com>
2024-03-04 19:27:29 +0530

    validation for sokets

--- backend/src/polls/polls.gateway.ts
+++ backend/src/polls/polls.gateway.ts
@@ -1,11 +1,27 @@
 import { Logger } from '@nestjs/common';
-import { WebSocketGateway, OnGatewayInit} from '@nestjs/websockets'
+import { WebSocketGateway, OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect, WebSocketServer} from '@nestjs/websockets'
+import { Socket, Namespace } from "socket.io"
 
 @WebSocketGateway({namespace: 'polls'})
-export class PollsGateway implements OnGatewayInit {
+export class PollsGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
     private readonly logger = new Logger(PollsGateway.name)
+    
+    @WebSocketServer() io: Namespace;
+
     afterInit() {
         this.logger.log('Websocket initialized');
     }
 
+    handleConnection(client: any) {
+        const sockets = this.io.sockets;
+        this.io.emit("hello", "OOOOOOOOOOO")
+        this.logger.log("ws client connected with socket id: ", client.id, " userId: " + client.userId)
+        this.logger.log("Number of connected socket: ", sockets.size)
+    }
+
+    handleDisconnect(client: Socket) {
+        const sockets = this.io.sockets;
+        this.logger.log("ws client disconnected with id: ", client.id)
+        this.logger.log("Number of connected socket: ", sockets.size)
+    }
 }
--- backend/src/socket-adapter.ts
+++ backend/src/socket-adapter.ts
@@ -1,11 +1,12 @@
-import { INestApplicationContext } from '@nestjs/common';
+import { ForbiddenException, INestApplicationContext } from '@nestjs/common';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { ServerOptions } from 'socket.io';
 import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 export class SocketAdapter extends IoAdapter {
     constructor(
-        app: INestApplicationContext,
+        private app: INestApplicationContext,
         private configService: ConfigService,
     ) {
         super(app)
@@ -23,8 +24,25 @@ export class SocketAdapter extends IoAdapter {
             ...options,
             cors
         };
-    
+        const jwtService = this.app.get(JwtService)
         const server = super.createIOServer(port, optionsWithCORS);
+        
+        server.of("polls").use(socketAuthMiddleware(jwtService))
         return server;
+    }
+}
+
+
+const socketAuthMiddleware = (JwtService) => (socket, next) => {
+    const token = socket.handshake.auth.token || socket.handshake.headers["token"]
+
+    try{
+        const payload = JwtService.verify(token)
+        socket.userId = payload.userId
+        socket.pollId = payload.pollId
+        socket.userName = payload.user
+        next()
+    }catch(err){
+        throw new ForbiddenException()
     }
 }
